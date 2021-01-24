1bcc2babd0a4944d3de91020f2eb8048fbd0afa5
Andy <andy27479@gmail.com>
2021-01-24 23:54:01 +0800

    finished JS03

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,7 +1,13 @@
 const inputs = document.querySelectorAll('.controls input');
 
-function handleUpdate(e) {
-
+function handleUpdate() {
+    /* this.dataset returns an object containing all data- attributes (i.e., data-sizing: px);
+    color slider has no data-sizing attribute */
+    const suffix = this.dataset.sizing || '';
+    /* documentElement -> root element
+    this.name -> input name; this.value -> input value */
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('change', handleUpdate));    // updates only when change is completed
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));  // updates simultaneously with mouse change
