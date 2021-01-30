27cbac3c5d499dba5ab1908d2fe06139505a8eda
Andy <andy27479@gmail.com>
2021-01-30 15:26:39 +0800

    completed JS09

--- 09 - Dev Tools Domination/script.js
+++ 09 - Dev Tools Domination/script.js
@@ -7,25 +7,52 @@ function makeGreen() {
 }
 
 // Regular
+console.log('Hello!');
 
 // Interpolated
+console.log('Hello, I am %s', 'Andy');
 
 // Styled
+console.log('%c Hello!', 'font-size: 20px; color: red;')
 
 // warning!
+console.warn('This is a warning!');
 
 // Error :|
+console.error('There is an error!');
 
 // Info
+console.info('Here is some info!');
 
 // Testing
+const p = document.querySelector('p');
+console.assert(p.classList.contains('na-class'), 'This is wrong!');  // <conditional test>, <message if false>
 
 // clearing
+// console.clear();
 
 // Viewing DOM Elements
+console.log(p);
+console.dir(p);  // dropdown of element
 
 // Grouping together
+dogs.forEach(dog => {
+    console.groupCollapsed(`${dog.name}`);
+    console.log(`This is ${dog.name}.`);
+    console.log(`${dog.name} is ${dog.age} years old.`);
+    console.groupEnd(`${dog.name}`);
+});
 
 // counting
+console.count('Element');
+console.count('Element');
+console.count('Element');   // console logs occurence of same element, i.e., Element: 3
 
 // timing
+console.time('fetching data');
+fetch('https://api.github.com/users/andy-techen')
+    .then(data => data.json())
+    .then(data => {
+        console.timeEnd('fetching data');
+        console.log(data);
+    });
