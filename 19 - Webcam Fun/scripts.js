08e385e59847c4d9f29a8bce9ee4cc00f6284466
Andy <andy27479@gmail.com>
2021-02-08 16:56:34 +0800

    completed JS19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,97 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })  // returns a promise
+        .then(localMediaStream => {
+            video.srcObject = localMediaStream;  // browsers are disabling createObjectURL()
+            video.play();
+        })
+        .catch((err) => {
+            console.error('Please allow access to webcam!', err);
+        })
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        // extract the pixels
+        let pixels = ctx.getImageData(0, 0, width, height);
+
+        // adjust the pixels
+        // pixels = redEffect(pixels);
+
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.5;
+
+        pixels = greenScreen(pixels);
+
+        // update new pixels
+        ctx.putImageData(pixels, 0, 0);
+    }, 16)
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'image');
+    link.innerHTML = `<img src=${data} alt="image"/>`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i] = pixels.data[i] + 100;  // r
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;  // g
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;  // b
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i];  // r
+        pixels.data[i + 500] = pixels.data[i + 1];  // g
+        pixels.data[i - 500] = pixels.data[i + 2];  // b
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach(input => {
+        levels[input.name] = input.value;
+    });
+
+    for (var i = 0; i < pixels.data.length; i + 4) {
+        red = pixels.data[i];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+    }
+
+    if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out if in between
+        pixels.data[i + 3] = 0;
+    }
+    return pixels;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
