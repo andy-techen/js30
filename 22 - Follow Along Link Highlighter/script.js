cb672660065720d3cef7752ab769ca5b8e0a96d5
Andy <andy27479@gmail.com>
2021-02-09 17:40:14 +0800

    completed JS22

--- 22 - Follow Along Link Highlighter/script.js
+++ 22 - Follow Along Link Highlighter/script.js
@@ -1,1 +1,14 @@
-// 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');   // added moving highlight span element
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+    const linkCoords = this.getBoundingClientRect();
+    highlight.style.width = `${linkCoords.width}px`;
+    highlight.style.height = `${linkCoords.height}px`;
+    highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top + window.scrollY}px)`;
+    // added scrollY to offset window scrolling
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', highlightLink));
